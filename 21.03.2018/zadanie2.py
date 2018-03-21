8558c6ce22557204f37c839c293bdb2243109a5e
Jarosław Rutkowski <choresize@gmail.com>
2018-03-21 12:51:37 +0100

    Add files via upload

---
+++ 21.03.2018/zadanie2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#encoding: utf-8
+import sys
+
+file=open(sys.argv[1])
+
+def funkcja(file):
+    
+    slownik={}
+    
+    napis2=str(napis).splitlines()
+    
+    for i in napis2:
+        slownik2=str(i).split(":")
+        slownik[slownik2[0]]=slownik2[1]
+
+    return slownik
+
+
+print funkcja("'k1:w1 \n k2:w2 \n k3:w3'")
+
+
