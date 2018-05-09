d3802a0d74f08edd58c80f242f2f44c72f1c1364
Jarosław Rutkowski <choresize@gmail.com>
2018-05-09 12:26:17 +0200

    Add files via upload

---
+++ 09.05.2018/zad2.py
@@ -0,0 +1,13 @@
+import Tkinter as tk
+import Tkfron as tkF
+
+root = tk.Tk()
+default_font=tkF.nameFront("TkDefaultFont")
+default_font.configure(size=48)
+root.option_add("*Front",default_font)
+L1=tk.Lable(root,text="L1",bg="green")
+L2=tk.Lable(root,text="L2",bg="red")
+L3=tk.Lable(root,text="L3",bg="blue")
+
+L1.
+root.mainloop()
